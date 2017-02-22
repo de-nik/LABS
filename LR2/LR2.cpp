31ac7a0a1e5c1b18b33229b81b100bb8c00d93a3
Denis Nikulin <bmstu.nikulin@yandex.ru>
2017-02-23 02:27:04 +0300

    Update LR2.cpp

--- LR2/LR2.cpp
+++ LR2/LR2.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <string>
-class TExh 
+class TExh
 {
 private:
-	int *Value, *Time, *T_V, Numb;
+	int *Value, *Time, Numb;
 	std::string Title;
 	TExh *a;
 public:
-	TExh() 
+	TExh()
 	{
 		Value = nullptr;
 		Time = nullptr;
 		a = nullptr;
-		T_V = nullptr;
 		Numb = 0;
 		Title = "\0";
 	}
-	TExh(const int Number) 
+	TExh(const int Number)
 	{
 		Numb = Number;
 		Value = new int[Numb];
-		T_V = new int[Numb];
 		Time = new int[Numb];
 		a = new TExh[Numb];
 	}
@@ -29,19 +27,6 @@ public:
 		a[i].Title = *c_title;
 		Value[i] = c_value;
 		Time[i] = c_time;
-		T_V[i] = Time[i] * Value[i];
-	}
-	int MAX()const{
-		int max = T_V[0], n = 0;
-		for (int i = 0; i < Numb; ++i)
-		{
-			if (T_V[i] > max)
-			{
-				max = T_V[i];
-				n = i;
-			}		
-		}
-		return n;
 	}
 	void AverageValue(int n) const
 	{
@@ -51,16 +36,27 @@ public:
 			std::cout << "[" << i + 1 << "] " << "Title = " << a[i].Title << " | " << "Value = " << Value[i] << " | " << "Av. Value = " << Value[i] * Time[i] << std::endl;
 		}
 		std::cout << "------------------------------------------------------" << std::endl << "max ->" << std::endl;
-		std::cout << "[" <<  n + 1 << "] " << "Title = " << a[n].Title << " | " << "Value = " << Value[n] << " | " << "Av. Value = " << T_V[n] << std::endl;
+		std::cout << "[" << n + 1 << "] " << "Title = " << a[n].Title << " | " << "Value = " << Value[n] << " | " << "Av. Value = " << Value[n] * Time[n] << std::endl;
 	}
-	~TExh() 
+	~TExh()
 	{
 		delete[]Value;
 		delete[]Time;
-		delete[]T_V;
 	}
 };
-int main() 
+int MAX(const int *T_V, const int Numb){
+	int max = T_V[0], n = 0;
+	for (int i = 0; i < Numb; ++i)
+	{
+		if (T_V[i] > max)
+		{
+			max = T_V[i];
+			n = i;
+		}
+	}
+	return n;
+}
+int main()
 {
 	int Value;
 	std::cout << "Enter the number of exhibits: " << std::endl;
@@ -69,13 +65,16 @@ int main()
 	TExh obj(Number);
 	std::cout << "Enter title, value of exhibits and time of exposure: " << std::endl;
 	int Time;
+	int *T_V = new int[Number];
 	std::string Title;
-	for (int i = 0; i < Number; ++i) 
+	for (int i = 0; i < Number; ++i)
 	{
 		std::cout << "[" << i + 1 << "]" << std::endl;
 		std::cin >> Title >> Value >> Time;
 		obj.Enter(&Title, Value, Time, i);
+		T_V[i] = Value * Time;
 	}
-	obj.AverageValue(obj.MAX());
+	obj.AverageValue(MAX(T_V, Number));
+	delete[]T_V;
 	system("pause");
 }
