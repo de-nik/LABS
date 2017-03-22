1dc2a9212487d47e1d0b995d257a2a915dbc9910
Denis Nikulin <bmstu.nikulin@yandex.ru>
2017-03-22 23:41:44 +0300

    Update Sort.cpp

--- M2/sort/Sort.cpp
+++ M2/sort/Sort.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-template<typename T> 
-void swap(T *Left, T *Right)
-{	
-	T Tmp = *Left;
-	*Left = *Right;
-	*Right = Tmp;
+template<typename T>
+void swap(T &Left, T &Right)
+{
+	T Tmp = Left;
+	Left = Right;
+	Right = Tmp;
 }
 
 template <typename IT>
-void sort(IT* a, IT* b)
+void Sort(IT a, IT b)
 {
-	for (IT* i = a; i < b-2; ++i)
+	for (IT i = a; i < b - 1; ++i)
 	{
-		for (IT* j = a; j < b-2; j++)
+		for (IT j = a; j < b - 1; j++)
 		{
-			if (*(j+1) < *j) swap(j+1, j); 
+			if (*(j + 1) < *j) swap(*(j + 1), *j);
 		}
 	}
 }
@@ -24,8 +24,8 @@ void sort(IT* a, IT* b)
 int main()
 {
 
-	int arr[] = { 9, 8, 7, 6, 5 };
-	sort(arr, arr + 3);
+	int arr[] = { 123, 3, 0, 13212, -100 };
+	Sort(arr, arr + 3);
 	for (int i = 0; i < 5; ++i)
 	{
 		std::cout << arr[i] << " ";
@@ -33,7 +33,7 @@ int main()
 	std::cout << std::endl;
 
 	std::vector<double> vec = { 123.0, 3.0, 0.0, 13212.0, -100.0 };
-	sort(vec.begin(), vec.end());
+	Sort(vec.begin(), vec.end());
 	for (int i = 0; i < 5; ++i)
 	{
 		std::cout << std::fixed << vec[i] << " ";
@@ -41,4 +41,3 @@ int main()
 	std::cout << std::endl;
 
 }
-
