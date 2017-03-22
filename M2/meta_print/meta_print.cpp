ff8d46ca190535d2db489672954ea542313d2585
Denis Nikulin <bmstu.nikulin@yandex.ru>
2017-03-22 23:24:26 +0300

    Update meta_print.cpp

--- M2/meta_print/meta_print.cpp
+++ M2/meta_print/meta_print.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <iterator>
-#include <algorithm>
-#include <vector>
-template <typename T, typename U>
-T *meta_print(T *vec, U variable)
-{
-	(*vec).insert((*vec).end(), ++variable);
-	if (variable < 100)
-		meta_print(vec, variable);
-	else return vec;
-}
-
-int main() 
-{
-	std::vector <int> vec;
-	meta_print(&vec, -1);
-	std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
-	std::cout << std::endl;
-}
++#include <iostream>
++#include <iterator>
++#include <algorithm>
++#include <vector>
++template <typename T, typename U>
++T meta_print(T vec, U variable)
++{
++	(*vec).insert((*vec).end(), ++variable);
++	if (variable < 100)
++		meta_print(vec, variable);
++	else return vec;
++}
++
++int main() 
++{
++	std::vector <int> vec;
++	meta_print(&vec, -1);
++	std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
++	std::cout << std::endl;
++}
 
