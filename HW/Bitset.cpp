e2f94e508f9cd7c4a81ff45fac61e952ea0e8cfb
Denis Nikulin <bmstu.nikulin@yandex.ru>
2017-04-12 04:47:46 +0300

    Update Bitset.cpp
    //To_do
    size = 4 < 2 ?
    cast_exception to ulong
    std::ostream
    make it at the day, not in 11 pm

--- HW/Bitset.cpp
+++ HW/Bitset.cpp
@@ -1,144 +1,165 @@
 #include <iostream>      
 #include <string>         
 #include <bitset>         
-#include <vector>
 
-class CastException : public std::exception
+class cast_exception : public std::exception
 { };
 
-class Out_of_range : public  CastException
+class out_of_range : public  cast_exception
 { };
 
 template <size_t N>
-class Bitset {
-	bool *Ptr;
-	size_t Size, my_pos, Count;
+class Bitset
+{
+	bool *ptr;
 public:
 	Bitset()
-		: Size(N)
-		, my_pos(0)
-		, Count(0)
+		: my_pos(0)
 	{
-		Ptr = new bool[Size];
-		for (size_t i(0); i < Size; ++i)
-			Ptr[i] = false;
+		ptr = new bool[N];
+		for (size_t i = 0; i < N; ++i)
+			ptr[i] = false;
 	}
 
 	Bitset(unsigned long long value)
-		: Size(N)
-		, my_pos(0)
-		, Count(0)
 	{
-		Ptr = new bool[Size];
+		ptr = new bool[N];
 		unsigned long long t = value;
-		for (int i = Size - 1; i >= 0; i--)
+		for (int i = N - 1; i >= 0; i--)
 		{
 			if (t % 2)
 			{
-				Ptr[i] = true;
-				++Count;
+				ptr[i] = true;
 			}
 			else
-				Ptr[i] = false;
+				ptr[i] = false;
 			t = t / 2;
 		}
 	}
 
-	Bitset(std::string str)
-		: Size(N)
-		, my_pos(0)
-		, Count(0)
+	Bitset(const std::string &str)
 	{
-		Ptr = new bool[Size];
-		for (int i = Size - 1, j = str.Size() - 1;
-			i >= Size - str.Size();
+		ptr = new bool[N];
+		for (int i = N - 1, j = str.size() - 1;
+			i >= N - str.size();
 			--i, --j)
 		{
 			if ((str[j] - '0') == 1)
 			{
-				Ptr[i] = true;
-				++Count;
+				ptr[i] = true;
 			}
 			else
 			{
-				Ptr[i] = false;
+				ptr[i] = false;
 			}
 		}
-		for (int i = Size - str.Size() - 1; i >= 0; i--)
-			Ptr[i] = 0;
+		for (int i = N - str.size() - 1; i >= 0; i--)
+		{
+			ptr[i] = 0;
+		}
+	}
+
+	Bitset(const std::string &str, size_t number)
+	{
+		if (number <= str.size())
+		{
+			ptr = new bool[N];
+			for (int i = N - 1, j = str.size() - 1;
+				i >= N - number;
+				--i, --j)
+			{
+				if ((str[j] - '0') == 1)
+				{
+					ptr[i] = true;
+				}
+				else
+				{
+					ptr[i] = false;
+				}
+			}
+			for (int i = N - 1 - number; i >= 0; i--)
+			{
+				ptr[i] = 0;
+			}
+		}
+		else
+		{
+			throw out_of_range();
+		}
+	}
+
+	Bitset(const std::string &str, size_t number, char x)
+	{
+		if (number <= str.size())
+		{
+			ptr = new bool[N];
+			for (int i = N - 1, j = str.size() - 1;
+				i >= N - number;
+				--i, --j)
+			{
+				if (str[j] == x || (str[j] - '0') == 1)
+				{
+					ptr[i] = true;
+				}
+				else
+				{
+					ptr[i] = false;
+				}
+			}
+			for (int i = N - 1 - number; i >= 0; i--)
+			{
+				ptr[i] = 0;
+			}
+		}
+		else
+		{
+			throw out_of_range();
+		}
 	}
 
 	Bitset(const Bitset<N> &rhs)
 	{
-		Size = rhs.Size;
-		Ptr = new bool[Size];
-		for (size_t i = 0; i < Size; ++i)
-			memcpy(Ptr, rhs.Ptr, Size * sizeof(bool));
-		Count = rhs.Count;
-		my_pos = rhs.my_pos;
-	}
-
-	Bitset operator[](size_t pos)
-	{
-		if (pos > Size || pos < 0)
-			throw Out_of_range();
-		else
-		{
-			my_pos = Size - 1 - pos;
-			return *this;
-		}
-	}
-
-	bool operator==(const Bitset<N>& rhs) const
-	{
-		for (int i = Size - 1; i >= 0; i--)
-		{
-			if (Ptr[i] != rhs.Ptr[i])
-				return false;
-		}
-		return true;
-	}
-
-	bool operator!=(const Bitset<N>& rhs) const
-	{
-		for (int i = Size - 1; i >= 0; i--)
-		{
-			if (Ptr[i] != rhs.Ptr[i])
-				return true;
-		}
-		return false;
+		ptr = new bool[N];
+		for (size_t i = 0; i < N; ++i)
+			memcpy(ptr, rhs.ptr, N * sizeof(bool));
 	}
 
 	size_t size()
 	{
-		return Size;
+		return N;
 	}
 
 	size_t count()
 	{
-		return Count;
+		size_t temp_w = 0;
+		for (size_t i = 0; i < N; ++i)
+		{
+			if (ptr[i])
+				++temp_w;
+		}
+		return temp_w;
 	}
 
-	Bitset<N>& set(size_t pos)
+	Bitset<N>& set(size_t pos, bool val = true)
 	{
-		if (pos > Size || pos < 0)
-			throw Out_of_range();
+		if (pos > N || pos < 0)
+		{
+			throw out_of_range();
+		}
 		else
 		{
-			Ptr[Size - 1 - pos] = true;
-			++Count;
+			ptr[N - 1 - pos] = val;
 			return *this;
 		}
 	}
 
 	Bitset<N>& set()
 	{
-		for (size_t i(0); i < Size; ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
-			if (Ptr[i])
+			if (ptr[i])
 			{
-				Ptr[i] = true;
-				++Count;
+				ptr[i] = true;
 			}
 		}
 		return *this;
@@ -146,41 +167,41 @@ public:
 
 	Bitset<N>& reset(size_t pos)
 	{
-		if (pos > Size || pos < 0)
-			throw Out_of_range();
+		if (pos > N || pos < 0)
+		{
+			throw out_of_range();
+		}
 		else
 		{
-			Ptr[Size - 1 - pos] = false;
-			--Count;
+			ptr[N - 1 - pos] = false;
 			return *this;
 		}
 	}
 
 	Bitset<N>& reset()
 	{
-		for (size_t i(0); i < Size; ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
-			Ptr[i] = false;
-			--Count;
+			ptr[i] = false;
 		}
 		return *this;
 	}
 
 	Bitset<N>& flip(size_t pos)
 	{
-		if (pos > Size || pos < 0)
-			throw Out_of_range();
+		if (pos > N || pos < 0)
+		{
+			throw out_of_range();
+		}
 		else
 		{
-			if (Ptr[Size - 1 - pos])
+			if (ptr[N - 1 - pos])
 			{
-				Ptr[Size - 1 - pos] = false;
-				--Count;
+				ptr[N - 1 - pos] = false;
 			}
 			else
 			{
-				Ptr[Size - 1 - pos] = true;
-				++Count;
+				ptr[N - 1 - pos] = true;
 			}
 			return *this;
 		}
@@ -188,50 +209,25 @@ public:
 
 	Bitset<N>& flip()
 	{
-		for (size_t i(0); i < Size; ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
-			if (Ptr[i])
+			if (ptr[i])
 			{
-				Ptr[i] = false;
-				--Count;
+				ptr[i] = false;
 			}
 			else
 			{
-				Ptr[i] = true;
-				++Count;
+				ptr[i] = true;
 			}
-		}
-		return *this;
-	}
-
-	Bitset& operator =(Bitset<N> &rhs)
-	{
-		Ptr = rhs.Ptr;
-		Size = rhs.Size;
-		Count = rhs.Count;
-		return *this;
-	}
-
-	Bitset& operator =(bool x)
-	{
-		if (x)
-		{
-			Ptr[my_pos] = true;
-			++Count;
-		}
-		else
-		{
-			Ptr[my_pos] = false;
-			--Count;
 		}
 		return *this;
 	}
 
 	bool all() const
 	{
-		for (size_t i(0); i < Size; ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
-			if (!Ptr[i])
+			if (!ptr[i])
 				return false;
 		}
 		return true;
@@ -239,9 +235,9 @@ public:
 
 	bool any() const
 	{
-		for (size_t i(0); i < Size; ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
-			if (Ptr[i])
+			if (ptr[i])
 				return true;
 		}
 		return false;
@@ -249,62 +245,17 @@ public:
 
 	bool none() const
 	{
-		for (size_t i(0); i < Size; ++i)
-		{
-			if (Ptr[i])
-				return false;
-		}
-		return true;
-	}
-
-	Bitset<N>& operator&=(const Bitset<N>& other)
-	{
-		for (size_t i(0); i < Size; ++i)
-			Ptr[i] = Ptr[i] & other.Ptr[i];
-		return *this;
-	}
-
-	Bitset<N> operator&(const Bitset<N>& rhs)
-	{
-		return Bitset<N>(lhs) &= rhs;
-	}
-
-	Bitset<N>& operator|=(const Bitset<N>& other)
-	{
-		for (size_t i(0); i < Size; ++i)
-			Ptr[i] = Ptr[i] | other.Ptr[i];
-		return *this;
-	}
-
-	Bitset<N> operator|(const Bitset<N>& rhs)
-	{
-		return Bitset<N>(lhs) |= rhs;
-	}
-
-	Bitset<N>& operator^=(const Bitset<N>& other)
-	{
-		for (size_t i(0); i < Size; ++i)
-			Ptr[i] = Ptr[i] ^ other.Ptr[i];
-		return *this;
-	}
-
-	Bitset<N> operator^(const Bitset<N>& rhs)
-	{
-		return Bitset<N>(lhs) ^= rhs;
-	}
-
-	Bitset<N> operator~() const
-	{
-		Bitset<N> a(*this);
-		return a.flip();
+		if (all())
+			return false;
+		else return true;
 	}
 
 	std::string to_string() const
 	{
 		std::string data;
-		for (size_t i(0); i < Size; ++i)
+		for (size_t i = 0; i < N; ++i)
 		{
-			if (Ptr[i] == 1)
+			if (ptr[i] == 1)
 				data.push_back('1');
 			else
 				data.push_back('0');
@@ -312,15 +263,152 @@ public:
 		return data;
 	}
 
+	unsigned long to_ulong() const
+	{
+		unsigned long a = 0;
+		for (size_t i = 0; i < N; ++i)
+		{
+			if (ptr[N - 1 - i])
+			{
+				a = pow(2, i) + a;
+			}
+		}
+		return a;
+	}
+
 	bool test(size_t pos) const
 	{
-		if (pos > Size || pos < 0)
-			throw Out_of_range();
+		if (pos > N || pos < 0)
+		{
+			throw out_of_range();
+		}
 		else return true;
 	}
 
 	~Bitset()
 	{
-		delete[] Ptr;
+		delete[] ptr;
 	}
+
+	constexpr bool operator[](std::size_t pos) const
+	{
+		if (pos > N || pos < 0)
+		{
+			throw out_of_range();
+		}
+		else
+		{
+			return ptr[N - pos - 1];
+		}
+	}
+
+	bool& operator[](std::size_t pos)
+	{
+		if (pos > N || pos < 0)
+		{
+			throw out_of_range();
+		}
+		else
+		{
+			return ptr[N - pos - 1];
+		}
+	}
+
+	Bitset& operator =(Bitset<N> &rhs)
+	{
+		if (*this != rhs)
+		{
+			ptr = rhs.ptr;
+			N = rhs.N;
+			return *this;
+		}
+		else
+		{
+			return *this;
+		}
+	}
+
+	Bitset& operator =(bool x)
+	{
+		if (*this != true)
+		{
+			*this = true;
+			return *this;
+		}
+		else
+		{
+			return *this;
+		}
+	}
+
 };
+
+template <size_t N>
+Bitset<N>& operator&=(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	for (size_t i(0); i < N; ++i)
+		lhs.ptr[i] = lhs.ptr[i] & other.ptr[i];
+	return *this;
+}
+
+template <size_t N>
+Bitset<N> operator&(const Bitset<N>& rhs)
+{
+	return Bitset<N>(lhs) &= rhs;
+}
+
+template <size_t N>
+Bitset<N>& operator|=(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	for (size_t i(0); i < N; ++i)
+		lhs.ptr[i] = lhs.ptr[i] | other.ptr[i];
+	return *this;
+}
+
+template <size_t N>
+Bitset<N> operator|(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	return lhs |= rhs;
+}
+
+template <size_t N>
+Bitset<N>& operator^=(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	for (size_t i(0); i < N; ++i)
+		lhs.ptr[i] = lhs.ptr[i] ^ other.ptr[i];
+	return *this;
+}
+
+template <size_t N>
+Bitset<N> operator^(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	return lhs ^= rhs;
+}
+
+template <size_t N>
+Bitset<N> operator~(Bitset<N>& lhs)
+{
+	return lhs.flip();
+}
+
+template <size_t N>
+bool operator==(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	for (int i = N - 1; i >= 0; i--)
+	{
+		if (lhs.ptr[i] != rhs.ptr[i])
+			return false;
+	}
+	return true;
+}
+
+template <size_t N>
+bool operator!=(Bitset<N>& lhs, const Bitset<N>& other)
+{
+	for (int i = N - 1; i >= 0; i--)
+	{
+		if (lhs.ptr[i] != rhs.ptr[i])
+			return true;
+	}
+	return false;
+}
