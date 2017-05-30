a43a61291bda77340f4639b60fb9d8fd8e856c69
Denis Nikulin <bmstu.nikulin@yandex.ru>
2017-05-30 04:10:38 +0300

    Update LR1.cpp

--- M2/LR1/LR1.cpp
+++ M2/LR1/LR1.cpp
@@ -14,6 +14,8 @@ class OverFlow : public  CastException {
 class Symbol : public  CastException {
 };
 
+
+
 int intFromString(const char* data)
 {
 	int Int = 0, CountOfInt = 0;
@@ -36,6 +38,7 @@ int intFromString(const char* data)
 	{
 		throw OverFlow();
 	}
+
 	else
 	{
 		for (i = 0, j = CountOfInt - 1; i < CountOfInt; i++, j--)
@@ -52,6 +55,7 @@ int intFromString(const char* data)
 	return Int;
 }
 
+
 bool boolFromString(const char* data) {
 	if (data == "true")
 	{
@@ -64,6 +68,7 @@ bool boolFromString(const char* data) {
 	else throw Symbol();
 }
 
+
 float floatFromString(const char * data)
 {
 	double Int = 0;
@@ -71,7 +76,8 @@ float floatFromString(const char * data)
 	std::vector <float> Arr(0);
 	for (int i = 0; data[i] != '\0'; i++)
 	{
-		if (ispunct(data[i]) || isspace(data[i]) || isalpha(data[i])) {
+		if (ispunct(data[i]) || isspace(data[i]) || isalpha(data[i]))
+		{
 			if (data[i] != ',' && data[i] != '.')
 			{
 				throw Symbol();
@@ -89,8 +95,7 @@ float floatFromString(const char * data)
 		}
 	}
 	int i, j;
-	if (CountOfInt <= 10)
-	{
+	if (t == 0) t = CountOfInt;
 		for (i = 0, j = t - 1; i < t; i++, j--)
 		{
 			long double m = Arr[i] * pow(10, j) + Int;
@@ -99,6 +104,7 @@ float floatFromString(const char * data)
 				throw OverFlow();
 			}
 			else Int = Arr[i] * pow(10, j) + Int;
+			std::cout << Int << std::endl;
 		}
 		double b = 0;
 		for (i = t, j = CountOfInt - t; i < CountOfInt; i++, j--)
@@ -107,47 +113,5 @@ float floatFromString(const char * data)
 		}
 		b = b / pow(10, CountOfInt - t + 1);
 		Int += b;
-	}
-	else throw OverFlow();
 	return (float)Int;
-}
-
-int main()
-{
-	try {
-		std::cout << "FinalResult: " << intFromString("3") << std::endl;
-		std::cout << "FinalResult: " << intFromString("4566ghk") << std::endl;
-	}
-	catch (Symbol &exc)
-	{
-		std::cout << "	Error!" << std::endl;
-	}
-	catch (OverFlow &exc)
-	{
-		std::cout << "	Error!" << std::endl;
-	}
-
-	try
-	{
-		std::cout << "FinalResult: " << boolFromString("true") << std::endl;
-		std::cout << "FinalResult: " << boolFromString("11fgmvhm1101") << std::endl;
-	}
-	catch (Symbol &exc)
-	{
-		std::cout << "	Error!" << std::endl;
-	}
-
-	try
-	{
-		std::cout << "FinalResult: " << floatFromString("16777.89") << std::endl;
-		std::cout << "FinalResult: " << floatFromString("1fdgh.89dfg") << std::endl;
-	}
-	catch (Symbol &exc)
-	{
-		std::cout << "	Error!" << std::endl;
-	}
-	catch (OverFlow &exc)
-	{
-		std::cout << "	Error!" << std::endl;
-	}
 }
